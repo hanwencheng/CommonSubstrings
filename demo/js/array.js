11c540f3b5973f14fd2fc12f8cd6cd1208718158
Hanwen Cheng <heawen.cheng@gmail.com>
2014-09-16 15:09:27 +0200

    Create array.js

---
+++ demo/js/array.js
@@ -0,0 +1,7 @@
+define(function(){
+    array = ['stuttgartfeuerbach', 'stuttgartbahnhof','deutschbahn', 'stuttgart','feuerbachhof','deutschmannschaft','mannheim','stuart','bierhoff','JavawhatisJava','doyoulikeJavaScript','notfavouorofJavaScript'];
+//array = ['sss1','sss2','sss3','sss4','sss5','sss6','ssstuttgart','histuttgart'];
+//array = ['doyoulikeJavaScript','notfavouorofJavaScript','JavawhatisJava'];
+
+    return array;
+});
